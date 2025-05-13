b1b0952ad3dfdefb8856f6c3bd2d72e4b9af0b2e
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-13 15:30:44 +0200

    fix: inline css

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,5 +1,5 @@
 <h1> Dashboard </h1>
-<div style="height:100px;width:300px;background-color:"red"">  
+<div style="height:100px;width:300px;background-color:red;">  
   <h2> Die genialen Lerntipps </h2>
   <span id="tip"> </span>
 </div>
