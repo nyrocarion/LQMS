76ed8a16611e133c8229323efcafad37b3583e09
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-13 15:01:51 +0200

    feat: added db call

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,1 +1,13 @@
 <h1> Dashboard </h1>
+<div style="height=100px;width=300px;background-color="red";>  
+  <h2> Die genialen Lerntipps </h2>
+  <span id="tip"> </span>
+</div>
+<script>
+  async function getTipFromDB () {
+    import { db } from '$lib/server/database';
+    const tip = await db.query('SELECT `tipps` FROM `content` WHERE `id`=1;');
+    document.getElementById("tip").innerHTML = tip;
+  }
+getTipFromDB();
+</script>
