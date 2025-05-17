78d3b21c310ec12a87bd3b32a16db55434e86f87
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:20:59 +0200

    fix: combined two load functions

--- lqms/src/routes/lqms/dashboard/+page.server.ts
+++ lqms/src/routes/lqms/dashboard/+page.server.ts
@@ -3,36 +3,24 @@ import type { PageServerLoad } from './$types';
 import { verifyJWT } from '$lib/server/jwt';
 import { db } from '$lib/server/database';
 
+// nur eine load Funktion erlaubt pro Datei :(
 export const load: PageServerLoad = async ({ cookies }) => {
+  // User Kram
   const token = cookies.get('authToken');
   const user = token && verifyJWT(token);
 
-  //if (!user) {
-  //  throw redirect(302, '/'); // zurück zur Startseite
-  //}
+  // Redirect zur Startseite
+  // if (!user) {
+  //   throw redirect(302, '/');
+  // }
 
-  return {
-    user
-  };
-
-  //Tab - Notizen
-
-  //Tab - Checkup
-
-  //Tab - Dashboard
-
-  //Tab - VL-Plan
-
-  //Tab Benutzerverwaltung (Admin only)
-
-};
-
-// gets a tip from the db to use in the dashboard
-export const load: PageServerLoad = async () => {
+  // Tip aus Datenbank holen
   const result = await db.query('SELECT `tipps` FROM `content` WHERE `id`=1;');
   const tip = result[0];
-  
+
+  // Zusammen zurückgeben (wird in dashboard geladen)
   return {
+    user,
     tip
   };
 };
