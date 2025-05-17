f0f7bc9738136854ca03bb5dd45ce402297348d5
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:23:16 +0200

    fix: export and access to variables

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,9 +1,18 @@
+// Daten aus dem Typscript Programm holen
 <script lang="ts">
+  export let user: any; 
   export let tip: string;
 </script>
 
-<h1 style="font-size: 100px;">Dashboard</h1>
+<h1>Dashboard</h1>
+
+// Wird nur angezeigt wenn ein User angemeldet ist
+{#if user}
+  <p>Willkommen zurück, {user.name}!</p>
+{/if}
+
 <div style="height:100px;width:300px;background-color:red;">  
   <h2>Die genialen Lerntipps</h2>
+  // beim Laden der Seite automatisch geladen
   <span>{tip}</span>
 </div>
