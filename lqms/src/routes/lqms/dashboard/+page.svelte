10293bbec76c2304f2de824fee08589170d27547
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:28:52 +0200

    fix: comments are now html format

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,4 +1,4 @@
-// Daten aus dem Typscript Programm holen
+<!--Daten aus dem Typscript Programm holen-->
 <script lang="ts">
   export let user: any; 
   export let tip: string;
@@ -6,13 +6,13 @@
 
 <h1>Dashboard</h1>
 
-// Wird nur angezeigt wenn ein User angemeldet ist
+<!--Wird nur angezeigt wenn ein User angemeldet ist-->
 {#if user}
   <p>Willkommen zurück, {user.name}!</p>
 {/if}
 
 <div style="height:100px;width:300px;background-color:red;">  
   <h2>Die genialen Lerntipps</h2>
-  // beim Laden der Seite automatisch geladen
+  <!--beim Laden der Seite automatisch geladen-->
   <span>{tip}</span>
 </div>
