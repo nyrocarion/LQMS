16cb89fe0c5f0dd9263a3603f582e5cf0ba3aa68
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:09:06 +0200

    fix: typo

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,4 +1,4 @@
-<h1 style="font-size: 100px;> Dashboard </h1>
+<h1 style="font-size: 100px;"> Dashboard </h1>
 <div style="height:100px;width:300px;background-color:red;">  
   <h2> Die genialen Lerntipps </h2>
   <span id="tip"> </span>
