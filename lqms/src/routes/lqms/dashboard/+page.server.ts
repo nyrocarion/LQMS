3ff06eacaa2f697cdd8746384f196aef1f980c05
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:37:18 +0200

    test: return a simple string to test if the connecting is working

--- lqms/src/routes/lqms/dashboard/+page.server.ts
+++ lqms/src/routes/lqms/dashboard/+page.server.ts
@@ -18,9 +18,13 @@ export const load: PageServerLoad = async ({ cookies }) => {
   const result = await db.query('SELECT `tipps` FROM `content` WHERE `id`=1;');
   const tip = result[0];
 
+  // Test Kram
+  const unga = "WirdGeladenAusTs";
+
   // Zusammen zurückgeben (wird in dashboard geladen)
   return {
     user,
-    tip
+    tip,
+    unga
   };
 };
