814122c808b06dd1b5914309ee25660189b5470d
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-17 17:14:02 +0200

    only loading tip from ts page

--- lqms/src/routes/lqms/dashboard/+page.svelte
+++ lqms/src/routes/lqms/dashboard/+page.svelte
@@ -1,11 +1,9 @@
-<h1 style="font-size: 100px;"> Dashboard </h1>
-<div style="height:100px;width:300px;background-color:red;">  
-  <h2> Die genialen Lerntipps </h2>
-  <span id="tip"> </span>
-</div>
-<script>
-  import { tip } from './+page.server.js';
-  console.log("script after import is working");
-  console.log(tip);
-  document.getElementById("tip").innerHTML = tip;
+<script lang="ts">
+  export let tip: string;
 </script>
+
+<h1 style="font-size: 100px;">Dashboard</h1>
+<div style="height:100px;width:300px;background-color:red;">  
+  <h2>Die genialen Lerntipps</h2>
+  <span>{tip}</span>
+</div>
