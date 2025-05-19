47e9ec940cba91263a98070d75c5739e347d189c
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-19 13:11:01 +0200

    fix: comments translated to german

--- lqms/src/lib/__tests__/tip.test.ts
+++ lqms/src/lib/__tests__/tip.test.ts
@@ -1,11 +1,12 @@
-import { load } from './page.server'; // Importiere deine Load-Funktion
-import { db } from '$lib/server/database'; // Der Datenbankzugriff, den wir mocken möchten
+import { load } from './page.server'; 
+import { db } from '$lib/server/database';
 
 // Mocken der db.query Methode
-jest.mock('$lib/server/database'); // Mock die DB-Abfrage
+jest.mock('$lib/server/database'); 
 
-describe('Server load function - tip', () => {
-  it('should return the tip from the database', async () => {
+// TODO: Value an der id = 1 ändern
+describe('Tip von der DB Laden', () => {
+  it('Sollte den Test Wert zurückgeben', async () => {
     (db.query as jest.Mock).mockResolvedValueOnce([
       [{ tipps: 'Unga Bunga' }] 
     ]);
@@ -14,8 +15,7 @@ describe('Server load function - tip', () => {
     expect(result[0][0].tipps).toBe('Unga Bunga');
   });
 
-  it('should return the default value when no tip is found', async () => {
-    // Mock für eine leere DB-Abfrage
+  it('Sollte den Standard Wert zurückgeben', async () => {
     (db.query as jest.Mock).mockResolvedValueOnce([]);
     const result = await db.query('SELECT `tipps` FROM `content` WHERE `id`=1;');
     expect(result[0][0].tipps).toBe('Kein Tipp gefunden');
