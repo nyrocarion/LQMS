28b73b154d2e30af5e054c609c0e16579d0f3d46
Nik <niklas@nbuchberger.de>
2025-06-06 19:05:55 +0200

    test: adding await to db query

--- lqms/src/routes/lqms/dashboard/+page.server.ts
+++ lqms/src/routes/lqms/dashboard/+page.server.ts
@@ -123,7 +123,7 @@ export const load: PageServerLoad = async ({ cookies }) => {
   // get tip from db
   // random value between 0 and 1, transform it so we get range(1,11)
   const id = Math.floor(Math.random() * 11) + 1;
-  const result = db.query('SELECT `tipps` FROM `content` WHERE `id` = ?', [id]);
+  const result = await db.query('SELECT `tipps` FROM `content` WHERE `id` = ?', [id]);
   // Gibt eine Ausgabe egal welcher Fall auftritt
   console.log("result from db call",result);
   const tip = (result[0] && result[0][0]?.tipps) ?? 'Kein Tipp gefunden';
