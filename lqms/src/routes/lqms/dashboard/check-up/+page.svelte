bf97ce6d8e04faddf3336e25fb3c0f7d65fc26d3
Nik <niklas@nbuchberger.de>
2025-06-09 14:36:52 +0200

    test: spoaghetti code for marco

--- lqms/src/routes/lqms/dashboard/check-up/+page.svelte
+++ lqms/src/routes/lqms/dashboard/check-up/+page.svelte
@@ -197,7 +197,7 @@
   };
 </script>
 
-<div class="parent app-container">
+<div style="height:100ch" class="parent app-container">
   <header class="nav">
     <ul>
       <li id="sessions"><a href="../dashboard/sessions/">Sessions</a></li>
