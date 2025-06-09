24972e63d591bc00eaa506a08253214c3c0b17c1
Nik <niklas@nbuchberger.de>
2025-06-09 14:35:46 +0200

    test: setting heigh with ch for session

--- lqms/src/routes/lqms/dashboard/sessions/+page.svelte
+++ lqms/src/routes/lqms/dashboard/sessions/+page.svelte
@@ -1,5 +1,5 @@
 <!-- Timer Block -->
-<div style="height:100%" class="app-container">
+<div style="height:100ch" class="app-container">
   <header class="nav">
     <ul>
       <li id="sessions"><a href="../dashboard/sessions/">Sessions</a></li>
